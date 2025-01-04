b511a3c33f9b73a0ad893a69ad89f783bdfdd6af
Atul Adhikari <atuladhikari01@gmail.com>
2025-01-04 15:03:05 +0545

    Modofied the ui of calculator

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,19 +1,42 @@
-const display=document.getElementById("display");
-function Display(input){
-    display.value+=input;
+let input = document.getElementById("inputBox");
+let buttons = document.querySelectorAll("button");
 
-}
-function clearDisplay(){
-    display.value="";
-    
-}
-function calculate(){
-    try{
-        display.value=eval(display.value)
-    }catch{
-        display.value="Error";
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      input.value = string;
+    } else if (e.target.innerHTML == "AC") {
+      string = "";
+      input.value = string;
+    } else if (e.target.innerHTML == "DEL") {
+      string = string.substring(0, string.length - 1);
+      input.value = string;
+    } else {
+      string += e.target.innerHTML;
+      input.value = string;
     }
-}
+  });
+});
 
-
-
+fetch("https://api.api-ninjas.com/v1/quotes", {
+  method: "GET",
+  headers: {
+    "X-Api-Key": "D84748MWXYKDqP993XFlkw==8BsLptqRntscY14U",
+    "Content-Type": "application/json",
+  },
+})
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data); // Handle the API response here
+  })
+  .catch((error) => {
+    console.error("Error: ", error.message); // Handle errors
+  });
